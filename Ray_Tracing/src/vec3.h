5b6cf1cbe5796ef22e19eba22d0287ec2f2ce4a6
Joadlaen <jackcullenmurphy1999@gmail.com>
2026-04-28 14:38:42 +0100

    created materials for use in ray tracing

--- Ray_Tracing/src/vec3.h
+++ Ray_Tracing/src/vec3.h
@@ -1,8 +1,8 @@
 #ifndef VEC3_H
 #define VEC3_H
 
-//#include <cmath>
-//#include <iostream>
+#include <cmath>
+#include <iostream>
 
 class vec3 {
 public:
@@ -19,30 +19,43 @@ public:
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
 
-    vec3& operator+=(const vec3& v) {
+    vec3& operator+=(const vec3& v) 
+    {
         e[0] += v.e[0];
         e[1] += v.e[1];
         e[2] += v.e[2];
         return *this;
     }
 
-    vec3& operator*=(double t) {
+
+    vec3& operator*=(double t) 
+    {
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
         return *this;
     }
 
-    vec3& operator/=(double t) {
+    vec3& operator/=(double t) 
+    {
         return *this *= 1 / t;
     }
 
-    double length() const {
+    double length() const 
+    {
         return std::sqrt(length_squared());
     }
 
-    double length_squared() const {
+    double length_squared() const 
+    {
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+
+    bool near_zero() const 
+    {
+        // Return true if the vector is close to zero in all dimensions.
+        auto s = 1e-8;
+        return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
     }
 
     static vec3 random() {
@@ -54,58 +67,80 @@ public:
     }
 };
 
+
+
+
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
 using point3 = vec3;
 
 
 // Vector Utility Functions
 
-inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) 
+{
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3& u, const vec3& v) {
+inline vec3 operator+(const vec3& u, const vec3& v) 
+{
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3& u, const vec3& v) {
+inline vec3 operator-(const vec3& u, const vec3& v) 
+{
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3& u, const vec3& v) {
+inline vec3 operator*(const vec3& u, const vec3& v) 
+{
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3& v) {
+inline vec3 operator*(double t, const vec3& v) 
+{
     return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator*(const vec3& v, double t) {
+inline vec3 operator*(const vec3& v, double t) 
+{
     return t * v;
 }
 
-inline vec3 operator/(const vec3& v, double t) {
+inline vec3 operator/(const vec3& v, double t) 
+{
     return (1 / t) * v;
 }
 
-inline double dot(const vec3& u, const vec3& v) {
+inline double dot(const vec3& u, const vec3& v) 
+{
     return u.e[0] * v.e[0]
         + u.e[1] * v.e[1]
         + u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3& u, const vec3& v) {
+inline vec3 reflect(const vec3& v, const vec3& n)
+{
+    return v - 2 * dot(v, n) * n;
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) 
+{
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
         u.e[2] * v.e[0] - u.e[0] * v.e[2],
         u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(const vec3& v) {
+inline vec3 unit_vector(const vec3& v) 
+{
     return v / v.length();
 }
 
-inline vec3 random_unit_vector() {
-    while (true) {
+inline vec3 random_unit_vector() 
+{
+    while (true) 
+    {
         auto p = vec3::random(-1, 1);
         auto lensq = p.length_squared();
         if (1e-160 < lensq && lensq <= 1)
